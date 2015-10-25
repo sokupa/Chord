c85690004374d78d2ba2a2e86b017ac3e292057f
sokupa <souravparmar@gmail.com>
2015-10-25 00:16:23 -0400

    version 1.1

--- src/main/scala/project3.scala
+++ src/main/scala/project3.scala
@@ -4,30 +4,27 @@ import java.security.MessageDigest
 import scala.util.Random
 import scala.collection.mutable.HashMap
 import akka.util.Timeout
-import java.math.BigInteger
+//import java.math.Longeger
 import scala.collection.immutable.{TreeMap, List}
 
 
-case class Firstjoin(nodeid : BigInt)
-case class Join(asknode:ActorRef)
-case class locateNode(node:ActorRef,nodeHash:BigInt)
-case class nodeLocated(predecessor:ActorRef,successor:ActorRef)
-case class Find_Finger(node:ActorRef,i:Int,start:BigInt)
-case class Found_Finger(i:Int,successor:ActorRef)
-case class Update_Finger(before:BigInt,i:Int,node:ActorRef,nodeHash:BigInt)
-case class Find(node:ActorRef,code:String, step:Int)
-case class Found(code: String,predecessor:ActorRef,successor:ActorRef,step:Int)
-case class setPredecessor(node:ActorRef)
-case class setSuccessor(node:ActorRef)
+case class m_Firstm_Join(nodeid : Long)
+case class m_Join(asknode:ActorRef)
+case class m_locateposition(node:ActorRef,nodeHash:Long)
+case class m_nodePosLocated(predecessor:ActorRef,successor:ActorRef)
+case class Find_Finger_Entry(node:ActorRef,i:Int,start:Long)
+case class Found_Finger_Entry(i:Int,successor:ActorRef)
+case class Update_Finger_Entry(before:Long,i:Int,node:ActorRef,nodeHash:Long)
+case class m_FindMsgTarget(node:ActorRef,code:String, Hop:Int)
+case class m_FoundMsgTarget(code: String,predecessor:ActorRef,successor:ActorRef,Hop:Int)
+case class m_setPredecessor(node:ActorRef)
+case class m_setSuccessor(node:ActorRef)
 case object Print
-//case class Joined()
-
-
 
 object Global {
-    var nodemap = new HashMap[BigInt, ActorRef]
+    var nodemap = new HashMap[Long, ActorRef]
      val max_len =4
-     val m_maxnodes:BigInt = BigInt(2).pow(max_len)
+     val m_maxnodes:Long = math.pow(2,max_len).toLong
      }
 
 object Main extends App{
@@ -46,9 +43,8 @@ object Main extends App{
   }
 
   class ChordNetwork(numofnodes:Int , numRequest: Int ) extends Actor {
-   // val starttime = system.currentTimeMillis()
-    var numjoined:Int = 0
-    var nodeID : BigInt = 0
+    var numm_Joined:Int = 0
+    var nodeID : Long = 0
     var node:ActorRef = null
     var totalHopCount = 0.0f
     var aggregate = 0.0f
@@ -57,27 +53,24 @@ object Main extends App{
     var refNode : ActorRef = null
     def receive = {
       case "createNetwork"=>{
-          println("Network create initiating \n")
-    
+          println("Network create initiating \n") 
 
        val system1 = ActorSystem("Worker")
-       var nodeset = scala.collection.mutable.Set[BigInt]()
+       var nodeset = scala.collection.mutable.Set[Long]()
                   while(nodeset.size != numofnodes)
                   {
                      nodeset += consistenthash(Random.nextInt(2000000))
-                     //println(nodeset.size)
                    }
                   for( i<-0 to numofnodes-1){  
                      Thread.sleep(100)          
                      try { 
                             nodeID = consistenthash(Random.nextInt(2000000))
-                            //nodeID = nodeset.toVector(i)
                             node = (system1.actorOf(Props(new Peer(nodeID)),name = getmyname(nodeID))) 
                                          // ...
                       } catch {
                         case e: Exception => {
                           if(retrycount < 10){
-                            println("Actor name clash tring max try again")
+                            println("Actor name clash trying max try again")
                           nodeID = consistenthash(Random.nextInt(2000000))
                             node = (system1.actorOf(Props(new Peer(nodeID)),name = getmyname(nodeID))) 
                             retrycount = retrycount +1
@@ -90,57 +83,40 @@ object Main extends App{
                         }
                         else
                         {    
-                         node ! Join(refNode)  
+                         node ! m_Join(refNode)  
                             Thread.sleep(100) 
                          } 
                     Global.nodemap.put(nodeID,node)
                   }
-                   // println(" i"+i)
-                    /*for( i<-0 to numofnodes-1){ 
-                     
-                       var nodeID = nodeset.toVector(i) 
-                    //   node =  Global.nodemap.get(nodeID)
-                    node =Global.nodemap(nodeID)
-                        if(i==0)
-                        {
-                          refNode = node
-                        }
-                        else
-                        {    
-                         node ! Join(refNode)  
-                            Thread.sleep(100) 
-                         } 
-                      }*/
-
-       }
+           }
       
-      case "Joined" => {
+      case "m_Joined" => {
 
-           numjoined = numjoined + 1
-            if(numjoined == numofnodes - 1){
-              println("joined " + numjoined)
+           numm_Joined = numm_Joined + 1
+            if(numm_Joined == numofnodes - 1){
+              println("m_Joined " + numm_Joined)
                    refNode ! Print
         }
 
       }
       case _ => 
     }
-   def consistenthash(index:Int): BigInt={
+   def consistenthash(index:Int): Long={
     var index1:String = index.toString
     var sha:String = ""
     sha = MessageDigest.getInstance("SHA-1").digest(index1.getBytes("UTF-8")).map("%02x".format(_)).mkString
 
-   val res:BigInt = Parsefirstmbits(sha)
+   val res:Long = Parsefirstmbits(sha)
    return res
  }
-   def getmyname (nodeId: BigInt):String ={
+   def getmyname (nodeId: Long):String ={
     return nodeId.toString
    }
-   def Parsefirstmbits(sha:String):BigInt={
+   def Parsefirstmbits(sha:String):Long={
     /*
     val loop:Int = 4 
     //println(loop)
-    var res: BigInt = 0
+    var res: Long = 0
     for(i<-0 to loop-1)
        res = (res << 4 ) | Character.digit(sha.charAt(i), 16);
     res = res >> 2
@@ -151,7 +127,7 @@ object Main extends App{
     
     val loop:Int = 1 
     //println(loop)
-    var res: BigInt = 0
+    var res: Long = 0
     for(i<-0 to loop-1)
        res = (res << 4 ) | Character.digit(sha.charAt(i), 16);
     
@@ -165,26 +141,29 @@ object Main extends App{
 
  }
 
-class Peer(nodeID : BigInt) extends Actor{
-  var fingerTable = new Array[FingerProp](Global.max_len) //2 ^ m  m =7
+class Peer(nodeID : Long) extends Actor{
+  var fingerTable = new Array[FingerProp](Global.max_len) 
      var Successor:ActorRef = self
      var Predecessor:ActorRef = self
      var refNode : ActorRef=null
-     var m_maxnodes : BigInt = Global.m_maxnodes
+     var m_maxnodes : Long = Global.m_maxnodes
      val m : Int = Global.max_len
+ def fingertableinit ():Unit={
   for(i <-0 until m) {
-    val start=(My_NodeID()+BigInt(2).pow(i))%(BigInt(2).pow(m))
-    val end=(My_NodeID()+BigInt(2).pow(i+1))%(BigInt(2).pow(m))
+    val start=(My_NodeID()+(math.pow(2, i)).toLong) % m_maxnodes
+    val end=(My_NodeID()+(math.pow(2, i+1) ).toLong)% m_maxnodes
     val interval= new Interval(true, start,end, false)
     fingerTable(i)= new FingerProp(start,interval,self,nodeID)
   }
-  def My_NodeID():BigInt={
+}
+  fingertableinit()
+  def My_NodeID():Long={
           val pattern = "([0-9]+)".r
           val pattern(num) = self.path.name
           //println(num.toInt)
           return num.toInt
   }
-  def My_NodeID(node:ActorRef):BigInt={
+  def My_NodeID(node:ActorRef):Long={
           val pattern = "([0-9]+)".r
           val pattern(num) = node.path.name
           //println(num.toInt)
@@ -192,7 +171,7 @@ class Peer(nodeID : BigInt) extends Actor{
   }
 
 
-  def closest_preceding_finger(id:BigInt): ActorRef = {
+  def closest_preceding_finger(id:Long): ActorRef = {
     val interval=new Interval(false,My_NodeID(),id,false)
     for(i <- m-1 to 0 by -1){
       if(interval.inValid(fingerTable(i).get_NodeID())) {
@@ -203,21 +182,21 @@ class Peer(nodeID : BigInt) extends Actor{
   }
 
 
-  def init_fingers():Unit = {
+  def init_finger_table():Unit = {
     fingerTable(0).setNode(Successor)
     fingerTable(0).Set_NodeID(My_NodeID(Successor))
-      //  println("Node    " +self+"init_fingers Entry at 0"+Successor)
+      //  println("Node    " +self+"init_finger_table Entry at 0"+Successor)
 
     for(i<-0 until m-1){
       val interval=new Interval(true,My_NodeID(),fingerTable(i).get_NodeID(),true)
       if(interval.inValid(fingerTable(i+1).getStart())) {
-           //println("Node    " +self +"init_fingers Entry at "+i+"is" +fingerTable(i).getNode())
+           //println("Node    " +self +"init_finger_table Entry at "+i+"is" +fingerTable(i).getNode())
         fingerTable(i+1).setNode(fingerTable(i).getNode())
         fingerTable(i+1).Set_NodeID(My_NodeID(fingerTable(i).getNode()))
       }
       else{
         if(refNode!=null){
-          refNode!Find_Finger(self,i+1,fingerTable(i+1).getStart())
+          refNode!Find_Finger_Entry(self,i+1,fingerTable(i+1).getStart())
         }
       }
     }
@@ -225,85 +204,85 @@ class Peer(nodeID : BigInt) extends Actor{
 
   def update_others():Unit = {
     for(i <- 0 to m-1) {
-      val position=(My_NodeID()-BigInt(2).pow(i)+BigInt(2).pow(m)+1)%BigInt(2).pow(m)
-      Successor!Update_Finger(position,i,self,My_NodeID())
+      val position=(My_NodeID()-(math.pow(2,i).toLong)+(math.pow(2,m)).toLong+1)% m_maxnodes
+      Successor!Update_Finger_Entry(position,i,self,My_NodeID())
     }
   }
 
 
   override def receive: Receive ={
 
-    case Join(asknode:ActorRef)=>{
-      println("Join")
+    case m_Join(asknode:ActorRef)=>{
+      println("m_Join")
       this.refNode=asknode
-      refNode!locateNode(self,My_NodeID)
-      sender ! "Joined"
+      refNode!m_locateposition(self,My_NodeID)
+      sender ! "m_Joined"
     }
 
-    case nodeLocated(predecessor:ActorRef,successor:ActorRef)=>{
+    case m_nodePosLocated(predecessor:ActorRef,successor:ActorRef)=>{
       this.Predecessor=predecessor
       this.Successor=successor
-      Predecessor!setSuccessor(self)
-      Successor!setPredecessor(self)
+      Predecessor!m_setSuccessor(self)
+      Successor!m_setPredecessor(self)
       println("Located")
       println(" self "+My_NodeID(self)+" Successor "+My_NodeID(Successor)+ " Predecessor "+ My_NodeID(Predecessor))
-      init_fingers()
+      init_finger_table()
       //println("Finger Initiated")
 
       update_others()
      // println("Others Updated")
-      sender ! "Joined"
+      sender ! "m_Joined"
     }
 
-    case Found_Finger(i:Int,successor:ActorRef)=>{
-      println("Node    " +My_NodeID(self) +"Found_Finger Entry at "+i+ "is "+My_NodeID(successor))
+    case Found_Finger_Entry(i:Int,successor:ActorRef)=>{
+      println("Node    " +My_NodeID(self) +"Found_Finger_Entry Entry at "+i+ "is "+My_NodeID(successor))
       this.fingerTable(i).setNode(successor)
       this.fingerTable(i).Set_NodeID(My_NodeID(successor))
     }
 
-    case locateNode(node:ActorRef,nodeHash:BigInt)=>{
+    case m_locateposition(node:ActorRef,nodeHash:Long)=>{
       val interval = new Interval(false, My_NodeID(), fingerTable(0).get_NodeID(), true)
       if(interval.inValid(nodeHash)){
-        node!nodeLocated(self,this.Successor)
+        node!m_nodePosLocated(self,this.Successor)
       }else{
 
         val target=closest_preceding_finger(nodeHash)
-        target!locateNode(node,nodeHash)
+        target!m_locateposition(node,nodeHash)
       }
     }
 
-    case Find_Finger(node:ActorRef,i:Int,start:BigInt)=>{
+    case Find_Finger_Entry(node:ActorRef,i:Int,start:Long)=>{
       val interval = new Interval(false, My_NodeID(), fingerTable(0).get_NodeID(), true)
       if(interval.inValid(start)){
-        node!Found_Finger(i,Successor)
+        node!Found_Finger_Entry(i,Successor)
       }else{
         val target=closest_preceding_finger(start)
-        target!Find_Finger(node,i,start)
+        target!Find_Finger_Entry(node,i,start)
       }
     }
-    case Update_Finger(before:BigInt,i:Int,node:ActorRef,nodeHash:BigInt)=>{
+    case Update_Finger_Entry(before:Long,i:Int,node:ActorRef,nodeHash:Long)=>{
       if(node!=self) {
         val interval1 = new Interval(false, My_NodeID(), fingerTable(0).get_NodeID(), true)
         if (interval1.inValid(before)) { //I am the node just before N-2^i
             val interval2=new Interval(false, My_NodeID(), fingerTable(i).get_NodeID(), false)
             if(interval2.inValid(nodeHash)){
-            println("Node    " +My_NodeID(self) +"Update_Finger Entry at "+i+ " is "+My_NodeID(node))
+            println("Node    " +My_NodeID(self) +"Update_Finger_Entry Entry at "+i+ " is "+My_NodeID(node))
               fingerTable(i).setNode(node)
               fingerTable(i).Set_NodeID(My_NodeID(node))
-              Predecessor!Update_Finger(My_NodeID(),i,node,nodeHash)
+              Predecessor!Update_Finger_Entry(My_NodeID(),i,node,nodeHash)
             }
         }else{
           val target=closest_preceding_finger(before)
-          target!Update_Finger(before,i,node,nodeHash)
+          target!Update_Finger_Entry(before,i,node,nodeHash)
         }
       }
     }
 
-    case setPredecessor(node:ActorRef)=>{
+    case m_setPredecessor(node:ActorRef)=>{
       this.Predecessor=node
     }
 
-    case setSuccessor(node:ActorRef)=>{
+    case m_setSuccessor(node:ActorRef)=>{
       this.Successor=node
     }
 
@@ -327,8 +306,8 @@ class Peer(nodeID : BigInt) extends Actor{
 
 
 
-class FingerProp(start: BigInt, interval: Interval, var node:ActorRef,var nodeID:BigInt){
-  def getStart(): BigInt = {
+class FingerProp(start: Long, interval: Interval, var node:ActorRef,var nodeID:Long){
+  def getStart(): Long = {
     return this.start
   }
   def getInterval(): Interval = {
@@ -337,74 +316,23 @@ class FingerProp(start: BigInt, interval: Interval, var node:ActorRef,var nodeID
   def getNode(): ActorRef = {
     return this.node
   }
-  def Set_NodeID(newnodeid :BigInt):Unit={
+  def Set_NodeID(newnodeid :Long):Unit={
      this.nodeID = newnodeid
   }
-  def get_NodeID():BigInt={
+  def get_NodeID():Long={
     return this.nodeID
   }
   def setNode(newNode:ActorRef):Unit ={
-   // println("Entry Added"+newNode)
     this.node=newNode
   }
 
   def print:String={
     return ("Start: %s, End: %s, Node: %d".format(start,interval.getEnd,get_NodeID()))
   }
-
-
-}; 
-
-/* Working Properly 
-object Main extends App{
-  val system=ActorSystem("system")
-  val node0=system.actorOf(Props(new Peer()), name = "0")
-
-  //Thread.sleep(1000)
-  val node2=system.actorOf(Props(new Peer()), name = "3")
-  val node1=system.actorOf(Props(new Peer()), name = "2")
-  val node4=system.actorOf(Props(new Peer()), name = "7")
-
-  //Thread.sleep(1000)
-
-  val node3=system.actorOf(Props(new Peer()), name = "6")
-
-
-  Thread.sleep(100)
-  node1!Join(node0)
-  Thread.sleep(100)
-  node3!Join(node0)
-  Thread.sleep(100)
-  node2!Join(node0)
-  Thread.sleep(100)
-  node4!Join(node0)
-
-
-
-  Thread.sleep(1000)
-
-  node0!Print
-  Thread.sleep(100)
-  node1!Print
-  Thread.sleep(100)
-  node2!Print
-  Thread.sleep(100)
-  node3!Print
-  Thread.sleep(100)
-  node4!Print
-
-  Thread.sleep(1000)
-
-  node4!Find(node4,"55555555555555555555555555555555",0)
-
-  Thread.sleep(1000)
-
-  system.shutdown()
 }
-*/
 
-class Interval(includeStart:Boolean,start:BigInt,end:BigInt, includeEnd:Boolean){
-  def inValid(nodetobefound:BigInt): Boolean = { 
+class Interval(includeStart:Boolean,start:Long,end:Long, includeEnd:Boolean){
+  def inValid(nodetobefound:Long): Boolean = { 
     if(start.equals(end))
     {
       if(includeStart.equals(false) && includeEnd.equals(false) && end.equals(nodetobefound))
@@ -441,7 +369,7 @@ class Interval(includeStart:Boolean,start:BigInt,end:BigInt, includeEnd:Boolean)
     else return false
   } 
 
-  def getEnd(): BigInt ={
+  def getEnd(): Long ={
     return end
   }
 }
